1fe0a330afe036c8e573a840a9bf7bac6db4d9d7
sammy404 <sam.morgan44@gmail.com>
2019-09-24 18:39:02 -0500

    moved sending and recieving to functions added ability to save and load data from local file

--- Lab 1/Python/Lab1.py
+++ Lab 1/Python/Lab1.py
@@ -1,42 +1,66 @@
 import serial
 import time
 
-arduino = serial.Serial('COM3', 9600, timeout=0)
+def saveFile(data):
+    file = open("data.dat", "w+")
+    for i in range(len(data)):
+        file.write(str(data[i]) + "\n")
+    file.close()
 
-print("Serial initialized...")
-#string = 'hello\n'
-#string = string.encode('utf-8')
-#test = 1
-#arduino.write(test)
-#test = arduino.read()
-#arduino.write(string)
-#print(test)
+def loadFile(data):
+    file = open("data.dat", "r")
+    content = file.readlines()
+    for i in content:
+        if(i != "\n"):
+            data.append(int(i))
+    file.close()
+    return data
 
-while True:
-    #print(arduino.is_open)
-    #print(arduino.readline())
-    string = 'hello\n'
-    string = string.encode('utf-8')
-    arduino.write(string)
+def send(message):
+    arduino.write(message)
     arduino.flush()
     while arduino.out_waiting != 0:
-        print("Sending data")
+        print("Sending" + message)
+
+def recieve():
+    while arduino.in_waiting == 0:
+                time.sleep(.001)
+    b = arduino.readline()
+    string_n = b.decode()
+    decoded_bytes = string_n.rstrip()
+    print(decoded_bytes)
+    return decoded_bytes
+
+#arduino = serial.Serial('COM3', 9600, timeout=0)
+
+while True:
     
-    try:
-        while arduino.in_waiting == 0:
-            time.sleep(.001)
-        b = arduino.readline()
-        string_n = b.decode()
-        decoded_bytes = string_n.rstrip()
-        print(decoded_bytes)
+    #graph null points
+    #possibly break if able to open port
+    if not arduino.is_open:
+
+        arduino = serial.Serial('COM3', 9600, timeout=0)
         
+    #graph data from sensor
+    else:
+        string = 'hello\n'
+        string = string.encode('utf-8')
+        arduino.write(string)
+        arduino.flush()
+        while arduino.out_waiting != 0:
+            print("Sending data")
         
-        
-        
-        
-        time.sleep(.1)
-    except Exception as e:
-        print(e)
-        break
+        try:
+            while arduino.in_waiting == 0:
+                time.sleep(.001)
+            b = arduino.readline()
+            string_n = b.decode()
+            decoded_bytes = string_n.rstrip()
+            print(decoded_bytes)
+
+            time.sleep(.1)
+        except Exception as e:
+            print(e)
+
         
     
